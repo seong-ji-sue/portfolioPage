5ab50cd4e3a862a0660ee2491b4a95cd33270e9e
jisue <tjdwltn121@gmail.com>
2022-02-23 01:04:33 +0900

    Modify: 프로젝트 데이터 추가 수정

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,9 +1,9 @@
 const LOCAL_URL = '/data';
 const GITHUB_URL = '/portfolioPage/data';
 
-export const MOCK_SKILLS_API = `${LOCAL_URL}/Skills.json`;
-export const MOCK_PROJECTS_API = `${LOCAL_URL}/Project.json`;
-export const MOCK_INFOS_API = `${LOCAL_URL}/Info.json`;
-export const MOCK_CAREERS_API = `${LOCAL_URL}/Career.json`;
-export const MOCK_ARCHIVINGS_API = `${LOCAL_URL}/Archiving.json`;
-export const MOCK_INTRODUCES_API = `${LOCAL_URL}/Introduce.json`;
+export const MOCK_SKILLS_API = `${GITHUB_URL}/Skills.json`;
+export const MOCK_PROJECTS_API = `${GITHUB_URL}/Project.json`;
+export const MOCK_INFOS_API = `${GITHUB_URL}/Info.json`;
+export const MOCK_CAREERS_API = `${GITHUB_URL}/Career.json`;
+export const MOCK_ARCHIVINGS_API = `${GITHUB_URL}/Archiving.json`;
+export const MOCK_INTRODUCES_API = `${GITHUB_URL}/Introduce.json`;
